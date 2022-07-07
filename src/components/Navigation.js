c729be733935a15cc4de0802d33069fded4c971a
soonyoungjung <107463495+soonyoungjung@users.noreply.github.com>
2022-07-08 00:07:35 +0900

    feat: 네비게이션 컴포넌트 추가

---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navigation from './components/Navigation';
+
+function App() {
+    return (
+        <div className="App">
+            <Navigation />
+            코맨토 쇼핑
+        </div>
+    );
+}
+
+
+export default App
+
